5b12ab803f5c23776569603b30517fea3352bc01
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-11-13 20:29:19 +0100

    Add pgn grammar

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-peg::parser!{
-  pub grammar list_parser() for str {
-    rule number() -> u32
-      = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
-
-    pub rule list() -> Vec<u32>
-      = "[" l:(number() ** ",") "]" { l }
-  }
-}
