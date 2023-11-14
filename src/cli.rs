d4f12a7ea7c3a00daa6c923b3f8cfc0adbc52548
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-11-14 23:31:39 +0100

    Enhance CLI

---
+++ src/cli.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version)]
+#[command(about = "pgn_parser - is a tool that allows you to parse chess games in PGN format.\nAuthor - Illia Shevchyk <illia.shevchyk@ukma.edu.ua>")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub(crate) command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Command which parse PGN
+    Parse {
+        ///provide input file
+        #[arg(short, long, value_name = "INPUT")]
+        input: Option<PathBuf>,
+        ///provide output file
+        #[arg(short, long, value_name = "OUTPUT")]
+        output: Option<PathBuf>,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-pub fn main() {
+extern crate pest;
+extern crate pest_derive;
 
+use clap::Parser;
+use crate::cli::{Cli, Commands};
+use pgn_parser::parse_pgn;
+mod cli;
+
+pub fn main() {
+    let cli = Cli::parse();
+
+
+    match &cli.command {
+        Some(Commands::Parse { input, output }) => {
+            if let Some(path_to_file) = input {
+                let file_content = std::fs::read_to_string(path_to_file).expect("Unable to read file");
+                let parsed_game = parse_pgn(&file_content).expect("TODO: panic message");
+                if let Some(output_path) = output {
+                    std::fs::write(output_path, parsed_game.to_string()).expect("Unable to write result");
+                } else {
+                    println!("{}", parsed_game.to_string());
+                }
+            } else {
+                println!("Missing path to file!");
+            }
+        }
+        None => {}
+    }
 }
